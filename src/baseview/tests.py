59fc81237fca59b60df7bcf17b97ed10ae215753
Mohamad Abuzaid <m.abuzaid.ali@gmail.com>
2020-05-02 13:19:03 +0200

    fix pytest

--- src/baseview/tests.py
+++ src/baseview/tests.py
@@ -1,15 +1,12 @@
-import unittest
 from django.test import TestCase
 from django.urls import reverse
 from django.utils.translation import activate
 
 # Create your tests here.
+
 class TestTemp(TestCase):
 
     def test_temp_uses_index_template(self):
         activate("en")
         response = self.client.get(reverse("dashboard"))
         self.assertTemplateUsed(response, "dashboard/mv_admin_dashboard.html")
-
-if __name__ == '__main__':
-    unittest.main()
